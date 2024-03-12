465798934c9dbecf75b6d2abd567403267bff60b
Nguyễn Anh Danh <danhanh.nguyen1643@gmail.com>
2024-03-12 09:38:58 +0700

    ..

--- backend/tests/test_users_dao.php
+++ backend/tests/test_users_dao.php
@@ -2,17 +2,18 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP File Execution</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP Test UserDAO</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    />
 </head>
 
 <body>
     <h1>PHP File Execution</h1>
     <?php
-    require_once 'backend/interfaces/dao_interface.php';
     require_once 'backend/models/user_model.php';
-    require_once 'backend/dao/database_connection.php';
     require_once 'backend/dao/users_dao.php';
     // Instantiate UserDAO
     $userDAO = UserDAO::getInstance();

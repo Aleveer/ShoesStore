4ec4b1da33e32ee1d04f9b744f141d360de836ed
Nguyễn Anh Danh <danhanh.nguyen1643@gmail.com>
2024-03-12 07:59:41 +0700

    added model classes, change structure to 3-tier.

---
+++ backend/models/product_model.php
@@ -0,0 +1,75 @@
+<?php
+class ProductModel
+{
+    private $id, $name, $categoryId, $price, $description, $image;
+
+    public function __construct($id, $name, $categoryId, $price, $description, $image)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->categoryId = $categoryId;
+        $this->price = $price;
+        $this->description = $description;
+        $this->image = $image;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getCategoryId()
+    {
+        return $this->categoryId;
+    }
+
+    public function setCategoryId($categoryId)
+    {
+        $this->categoryId = $categoryId;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+}

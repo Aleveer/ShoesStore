933fd38a72401ec3200dd971f6b31dc933c9b3f9
Nguyễn Anh Danh <danhanh.nguyen1643@gmail.com>
2024-04-08 13:07:47 +0700

    Remove unused files

--- frontend/templates/layouts/header.php
+++ frontend/templates/layouts/header.php
@@ -10,7 +10,7 @@ if (!defined('_CODE')) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo !empty($data['pageTitle']) ? ($data['pageTitle']) : 'Quản lí người dùng'; ?></title>
+    <title><?php echo !empty($data['pageTitle']) ? ($data['pageTitle']) : 'Home'; ?></title>
     <link rel="stylesheet" href="<?php echo _WEB_HOST_TEMPLATE ?>/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo _WEB_HOST_TEMPLATE ?>/css/style.css?ver=1">
     <link rel="stylesheet" href="<?php echo _WEB_HOST_TEMPLATE ?>/css/grid.css?ver=1">

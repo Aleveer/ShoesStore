284f863cdb75ac93127888dc33cb03d22c5277a1
Nguyễn Anh Danh <danhanh.nguyen1643@gmail.com>
2024-05-03 11:03:44 +0700

    Refactor code to fix password validation issue, improve error handling, and remove unnecessary password input name in account.view.php and product.view.php

--- frontend/templates/js/dashboard/add_sizeitem.js
+++ frontend/templates/js/dashboard/add_sizeitem.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
             }
 
             $.ajax({
-                url: 'http://localhost/frontend/index.php?module=dashboard&view=inventory.view', // replace with your server script URL
+                url: 'http://localhost/frontend/index.php?module=dashboard&view=inventory.view',
                 method: "POST",
                 dataType: "json",
                 data: {
